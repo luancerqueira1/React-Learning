088b1a55db61c472d7edf5ec85e2c959be9909fc
Luan Cerqueira <luangamacerqueira1@gmail.com>
2022-11-30 18:58:41 -0300

    save in localstorage wip

--- descomplica-projeto/src/App.js
+++ descomplica-projeto/src/App.js
@@ -4,13 +4,14 @@ import {
    Route,
    Routes,
 } from 'react-router-dom';
-import { saveFolder } from './services/PinService';
 import { HomePage } from './pages/Home/HomePage';
 import { MinhasPastasPage } from './pages/MinhasPastas/MInhasPastasPage';
 import { HeaderPartial } from './Partials/HeaderPartial/HeaderPartial';
+import { saveFolder , savePinInFolder} from './services/PinService';
 
 function App() {
-   saveFolder('luan');
+   // saveFolder('luan');
+   savePinInFolder('2345-554','luan')
    return (
       <BrowserRouter>
          <div className='App'>
@@ -21,7 +22,7 @@ function App() {
             </Routes>
          </div>
       </BrowserRouter>
-   );
-}
+   )
+};
 
 export default App;
--- descomplica-projeto/src/services/PinService.js
+++ descomplica-projeto/src/services/PinService.js
@@ -2,6 +2,11 @@
 const generateId = () => {
    return `${(Math.floor(Math.random() * 100_000)).toString(16)}-${(Math.floor(Math.random() * 100_000)).toString(16)}`;
 }
+
+const saveFolders = async (folders) => {
+      localStorage.setItem('folders', JSON.stringify(folders));
+}
+
 export const getFolders = async () => {
    return JSON.parse(localStorage.getItem('folders')) || [];
 }; 
@@ -17,6 +22,23 @@ export const saveFolder = async (folderName) => {
    };
 
    folders.push(newFolder);
-   localStorage.setItem('folders', JSON.stringify(folders))
+
+   await saveFolders(folders);
+
    return newFolder;
-}
+};
+
+export const savePinInFolder = async (folderId, pinId) => {
+   const folders = await getFolders();
+
+   const folderIndex = folders.findIndex(function (folder) {
+      return folder.id === folderId;
+   });
+   if (folderIndex !== -1) {
+      folders[folderIndex].pins.push(pinId)
+   }
+   
+   await saveFolders(folders);
+
+   return { ...folders[folderIndex] };
+};
